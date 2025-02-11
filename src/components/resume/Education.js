e86f66d0cfe377b2b15b1081af11c2fa1c98012a
Bhramarambha8660 <bhramarambhacb8660@gmail.com>
2025-02-11 12:44:14 +0530

    education caertification Update

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -47,31 +47,31 @@ const Education = () => {
 
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2022 - 2023</p>
+          <p className="text-sm text-designColor tracking-[4px]">2023-2024</p>
           <h2 className="text-3xl md:text-4xl font-bold">Certificates</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="React 18"
-            subTitle="Codedamn - Oct-2022"
+            title="MERN Stack with Blog Project"
+            subTitle="Codedamn - Jan-2024"
             result="View"
-            des="Learnt react and how to develop a dynamic web application from scratch !"
-            certref='https://codedamn.com/certificate/verify/c930bd7a08aac934c2b3da65601c3397a1f1384c'
+            des="A comprehensive course on building a full-stack blog application from scratch, covering backend and frontend development with deployment."
+            certref='https://codedamn.com/certificate/verify/f0591e8cb43aa12ad5e57654c438ac6aedc632aa'
 
           />
           <ResumeCard
-            title="Learn MongoDb and Node JS"
-            subTitle="Codedamn - Sept-2022"
+            title="Learn ReactJS by Building 5 Projects"
+            subTitle="Codedamn - Feb-2022"
             result="View"
-            des="I have learnt how to write queries in mongodb and using mongoose as the ORM for mongodb in Node JS."
-            certref='https://codedamn.com/certificate/verify/e5c2a4d4d28d42c845d5f00b12c07e126c2504eb'
+            des="A beginner-friendly course that teaches ReactJS by building projects, starting with the basics and progressing to more advanced applications."
+            certref='https://codedamn.com/certificate/verify/1bfd71f0ae2732be983dd49b2a1bf9448ed27b88'
           />
           <ResumeCard
-            title="Web Security"
-            subTitle="Codedamn - Sept-2022"
+            title="Tailwind CSS Crash Course"
+            subTitle="Codedamn - Jan-2022"
             result="view"
-            des="Learnt how to build a secured web app. Also learned using bcrypt and jwt for authentication and authorization."
-            certref="https://codedamn.com/certificate/verify/a28a175a7d74e26236fedde1d1710cf4f9e1f8ba"
+            des="A comprehensive course that teaches web design using Tailwind CSS and React, enabling you to build stylish, responsive websites efficiently."
+            certref="https://codedamn.com/certificate/verify/95716dab875994379646f7e62f0eec0a1515bbb1"
           />
         </div>
       </div>
